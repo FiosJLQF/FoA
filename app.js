27c0f90dcafa9cd339db1aa4845920c7a84cc96d
Jeff QF <JeffQF@fios.tech>
2020-09-10 13:21:24 -0700

    added Procfile

--- app.js
+++ app.js
@@ -5,21 +5,25 @@ const exphbs = require('express-handlebars');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const bodyParser = require('body-parser');
 //const eventRouter = express.Router();
 
 // Set up local variables and file locations
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));  // publicly-accessible files (such as images and css)
 
+// FINISH THIS LATER!
+//app.use(bodyParser.urlencoded({extended: false}));
+
 // A2 Config
 //app.use(express.static('/test/public'));  // publicly-accessible files (such as images and css)
+
 app.engine('handlebars', exphbs());
 app.set('views', 'views');  // HTML pages and templates, using EJS for templating
-//app.set('view engine', 'handlebars' );  // instead of EJS below?
 app.set('view engine', 'ejs');  // Sets the EJS engine
 
 // A2 Config
-app.use('/test', routes);  // imports the URL endpoint routes from index.js
+//app.use('/test', routes);  // imports the URL endpoint routes from index.js
 
 app.use('/', routes);  // imports the URL endpoint routes from index.js
 
