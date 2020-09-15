44b1dcc992bc79c056ad8ffc5854dd99893cef53
Jeff QF <JeffQF@fios.tech>
2020-09-15 09:33:22 -0700

    Test Heroku as Collaborator

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Import and require external libraries/files
 const compression = require('compression');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
 //const eventRouter = express.Router();
 
 // Set up local variables and file locations
@@ -15,20 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));  // publicly-accessible
 // FINISH THIS LATER!
 //app.use(bodyParser.urlencoded({extended: false}));
 
-// A2 Config
-//app.use(express.static('/test/public'));  // publicly-accessible files (such as images and css)
-
-app.engine('handlebars', exphbs());
 app.set('views', 'views');  // HTML pages and templates, using EJS for templating
 app.set('view engine', 'ejs');  // Sets the EJS engine
-
-// A2 Config
-//app.use('/test', routes);  // imports the URL endpoint routes from index.js
-
 app.use('/', routes);  // imports the URL endpoint routes from index.js
 
 // For local testing only
-const port = process.env.PORT || 3000;
 app.listen(port, function(err) {
     console.log(`The server is running on port ${port}`);
     console.log(__dirname);
