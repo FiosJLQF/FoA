f7937a5cf82b4c9e08b9e450b9d62b0a40d07cc5
Jeff QF <JeffQF@fios.tech>
2020-09-24 10:23:40 -0700

    test9

---
+++ models/scholarshipModels.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+const db = require('../db/db_config.js');
+
+///////////////////////////////////////////////////////////////////////////////////
+// a list of all active Scholarships (with Sponsor dimensional data)
+///////////////////////////////////////////////////////////////////////////////////
+const ScholarshipsActive = db.define('vwScholarshipsActive', {
+    ScholarshipID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    ScholarshipName: {
+        type: Sequelize.STRING
+    },
+    ScholarshipAward: {
+        type: Sequelize.STRING
+    },
+    ScholarshipDescription: {
+        type: Sequelize.STRING
+    },
+    Criteria_FieldOfStudyText: {
+        type: Sequelize.STRING
+    },
+    Criteria_Gender_FemaleOnly: {
+        type: Sequelize.STRING
+    },
+    SponsorID: {
+        type: Sequelize.INTEGER
+    }
+
+// Awards, Type, Description
+
+
+}, {
+    freezeTableName: true,
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: false,
+});
+
+module.exports = {
+    ScholarshipsActive
+};
