7252c173743a4f54e8cbf69c9adf295e083680e7
Jeff QF <JeffQF@fios.tech>
2020-12-17 14:49:05 -0700

    Added Auth0 Basic Authentication Functionality

--- app.js
+++ app.js
@@ -21,16 +21,57 @@ app.set('view engine', 'ejs');  // Sets the EJS engine
 //app.use('/', routes);  // imports the root folder URL endpoint routes from index.js
 app.use('/sponsorsearch', require('./routes/sponsorRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
 app.use('/scholarshipsearch', require('./routes/scholarshipRoutes'));  // imports the "scholarships" URL endpoint routes from scholarshipRoutes.js
+app.use('/portal', require('./routes/portalRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
 
+
+///////////////////////////////////////////////////////////////////////////////////
+// Auth0 Configuration
+///////////////////////////////////////////////////////////////////////////////////
+const { auth } = require('express-openid-connect');
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_CLIENT_SECRET, // a long, randomly-generated string stored in env
+  baseURL: process.env.APP_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_BASE_URL
+};
+app.use(auth(config)); // auth router attaches /login, /logout, and /callback routes to the baseURL
+app.get('/', (req, res) => { // req.isAuthenticated is provided from the auth router
+    res.redirect(req.oidc.isAuthenticated() ? '/portal/switchboard' : '/portal');
+  });
+//var session = require('express-session');
+//var sess = {
+//    secret: process.env.AUTH0_CLIENT_SECRET,
+//    cookie: {},
+//    resave: false,
+//    saveUninitialized: true
+//};
+//if (app.get('env') === 'production') {
+//    // Use secure cookies in production (requires SSL/TLS)
+//    sess.cookie.secure = true;
+//    // Uncomment the line below if your application is behind a proxy (like on Heroku)
+//    // or if you're encountering the error message:
+//    // "Unable to verify authorization request state"
+//    // app.set('trust proxy', 1);
+//}
+//app.use(session(sess));
+
+
+///////////////////////////////////////////////////////////////////////////////////
 // For local testing only
+///////////////////////////////////////////////////////////////////////////////////
 app.listen(port, function(err) {
     console.log(`The server is running on port ${port}`);
     console.log(__dirname);
     console.log(__dirname + '/public');
 })
-
 app.use((err, req, res, next) => {
     res.json(err);
 });
 
+
+///////////////////////////////////////////////////////////////////////////////////
+// Export to calling routines
+///////////////////////////////////////////////////////////////////////////////////
 module.exports = app;
---
+++ routes/portalRoutes.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const db = require('./../db/db_config.js');
+const Sponsors = require(path.join(__dirname, '../models/', 'sponsorModels.js'));
+const Scholarships = require(path.join(__dirname, '../models/', 'scholarshipModels.js'));
+
+// All routes that start with "/sponsorsearch"; since this is part of the definition in app.js
+// the "/" implies "/sponsorsearch".
+
+///////////////////////////////////////////////////////////////////////////////////
+// Sponsor Search page (no results) (i.e., https://.../sponsorsearch/)
+///////////////////////////////////////////////////////////////////////////////////
+router.get('/', async (request, response) => {
+
+    try {
+        // get a distinct list of Sponsors
+//        const sponsorsAll = await Sponsors.SponsorsAll.findAll();
+
+        // get a distinct list of Sponsors for the Search Criteria DDL
+//        const sponsorsDDL = await Sponsors.SponsorsDDL.findAll();
+
+        // get a distinct list of Sponsor Type(s) for the Search Criteria DDL
+//        const sponsortypesDDL = await Sponsors.SponsorTypeCategoriesDDL.findAll();
+
+        // get a distinct list of active Scholarships, ordered by Sponsor Name, Scholarship Name
+//        const scholarshipsActive = await Scholarships.ScholarshipsActive.findAll(
+//             {
+//            order: [
+//                ['ScholarshipName', 'ASC'],
+//                ['SponsorName', 'ASC'],
+//            ]
+//        }
+//        );
+//        console.log(`Scholarships (Active) Count: ${scholarshipsActive.length}`);
+
+        return response.render('portal', {
+//            sponsors:        sponsorsAll,
+//            sponsorsddl:     sponsorsDDL,
+//            sponsortypesddl: sponsortypesDDL,
+//            scholarships:    scholarshipsActive
+        })
+    } catch(err) {
+        console.log('Error:' + err);
+    }
+});
+
+router.get('/switchboard', async (request, response) => {
+
+        try {
+            // get a distinct list of Sponsors
+    //        const sponsorsAll = await Sponsors.SponsorsAll.findAll();
+    
+            // get a distinct list of Sponsors for the Search Criteria DDL
+    //        const sponsorsDDL = await Sponsors.SponsorsDDL.findAll();
+    
+            // get a distinct list of Sponsor Type(s) for the Search Criteria DDL
+    //        const sponsortypesDDL = await Sponsors.SponsorTypeCategoriesDDL.findAll();
+    
+            // get a distinct list of active Scholarships, ordered by Sponsor Name, Scholarship Name
+    //        const scholarshipsActive = await Scholarships.ScholarshipsActive.findAll(
+    //             {
+    //            order: [
+    //                ['ScholarshipName', 'ASC'],
+    //                ['SponsorName', 'ASC'],
+    //            ]
+    //        }
+    //        );
+    //        console.log(`Scholarships (Active) Count: ${scholarshipsActive.length}`);
+    
+            return response.render('switchboard', {
+    //            sponsors:        sponsorsAll,
+    //            sponsorsddl:     sponsorsDDL,
+    //            sponsortypesddl: sponsortypesDDL,
+    //            scholarships:    scholarshipsActive
+            })
+        } catch(err) {
+            console.log('Error:' + err);
+        }
+    });
+    
+///////////////////////////////////////////////////////////////////////////////////
+// export all routes
+///////////////////////////////////////////////////////////////////////////////////
+module.exports = router;
