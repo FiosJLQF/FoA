ad2864b99cb2a3bce0dc0f08ee2b34a173db6355
Jeff QF <JeffQF@fios.tech>
2020-12-18 13:40:01 -0700

    Auth0 Basics Implemented

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 require("dotenv").config();
+//const Auth0Strategy = require("passport-auth0");
+const expressSession = require("express-session");
+//const passport = require("passport");
+//const authRouter = require("./routes/auth");  // routes used by Auth0 services
+const { auth, requiresAuth } = require('express-openid-connect');
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Set up local variables and file locations
@@ -20,17 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));  // publicly-accessible
 
 // FINISH THIS LATER!
 //app.use(bodyParser.urlencoded({extended: false}));
-
-
-///////////////////////////////////////////////////////////////////////////////////
-// App Configuration
-///////////////////////////////////////////////////////////////////////////////////
-app.set('views', 'views');  // HTML pages and templates, using EJS for templating
-app.set('view engine', 'ejs');  // Sets the EJS engine
-//app.use('/', routes);  // imports the root folder URL endpoint routes from index.js
-app.use('/sponsorsearch', require('./routes/sponsorRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
-app.use('/scholarshipsearch', require('./routes/scholarshipRoutes'));  // imports the "scholarships" URL endpoint routes from scholarshipRoutes.js
-app.use('/portal', require('./routes/portalRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
 
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -48,19 +42,80 @@ if (app.get("env") === "production") {
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////
+// Passport Configuration
+///////////////////////////////////////////////////////////////////////////////////
+
+//const strategy = new Auth0Strategy(
+//    {
+//      domain: process.env.AUTH0_DOMAIN,
+//      clientID: process.env.AUTH0_CLIENT_ID,
+//      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+//      callbackURL: process.env.AUTH0_CALLBACK_URL
+//    },
+//    function(accessToken, refreshToken, extraParams, profile, done) {
+//      /**
+//       * Access tokens are used to authorize users to an API
+//       * (resource server)
+//       * accessToken is the token to call the Auth0 API or a secured third-party API
+//       * extraParams.id_token has the JSON Web Token
+//       * profile has all the information from the user
+//       */
+//      return done(null, profile);
+//    }
+//);
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// App Configuration
+///////////////////////////////////////////////////////////////////////////////////
+app.set('views', 'views');  // HTML pages and templates, using EJS for templating
+app.set('view engine', 'ejs');  // Sets the EJS engine
+//app.use('/', routes);  // imports the root folder URL endpoint routes from index.js
+app.use('/sponsorsearch', require('./routes/sponsorRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
+app.use('/scholarshipsearch', require('./routes/scholarshipRoutes'));  // imports the "scholarships" URL endpoint routes from scholarshipRoutes.js
+app.use('/portal', require('./routes/portalRoutes'));  // imports the "sponsors" URL endpoint routes from sponsorRoutes.js
+app.use(expressSession(session));  // uses the Session environment create above
+//passport.use(strategy);
+//app.use(passport.initialize());
+//app.use(passport.session());
+//passport.serializeUser((user, done) => {
+//    done(null, user);
+//});
+//passport.deserializeUser((user, done) => {
+//    done(null, user);
+//});
+// Creating custom middleware with Express
+//app.use((req, res, next) => {
+//    res.locals.isAuthenticated = req.isAuthenticated();
+//    next();
+//});
+//app.use("/", authRouter);
+app.use(
+    auth({
+      issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+      baseURL: process.env.BASE_URL,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      secret: process.env.SESSION_SECRET,
+      authRequired: false,  // set to false by default as some pages are public
+      auth0Logout: true,
+    })
+);
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.oidc.isAuthenticated();
+    next();
+});
+
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Auth0 Configuration
 ///////////////////////////////////////////////////////////////////////////////////
-const expressSession = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
-const { auth, requiresAuth } = require('express-openid-connect');
+/*
 const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_CLIENT_SECRET, // a long, randomly-generated string stored in env
-  baseURL: process.env.APP_BASE_URL,
+  baseURL: process.env.APP_DOMAIN,
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: process.env.AUTH0_BASE_URL
 };
@@ -68,39 +123,50 @@ app.use(auth(config)); // auth router attaches /login, /logout, and /callback ro
 app.get('/', (req, res) => { // req.isAuthenticated is provided from the auth router
     res.redirect(req.oidc.isAuthenticated() ? '/portal/switchboard' : '/portal');
   });
-//var session = require('express-session');
-//var sess = {
-//    secret: process.env.AUTH0_CLIENT_SECRET,
-//    cookie: {},
-//    resave: false,
-//    saveUninitialized: true
-//};
-//if (app.get('env') === 'production') {
-//    // Use secure cookies in production (requires SSL/TLS)
-//    sess.cookie.secure = true;
-//    // Uncomment the line below if your application is behind a proxy (like on Heroku)
-//    // or if you're encountering the error message:
-//    // "Unable to verify authorization request state"
-//    // app.set('trust proxy', 1);
-//}
-//app.use(session(sess));
+*/
 
 
-
-
-app.get('/switchboard', requiresAuth(), async (request, response) => {
-
+///////////////////////////////////////////////////////////////////////////////////
+// Routes
+///////////////////////////////////////////////////////////////////////////////////
+app.get('/switchboard', async (req, res) => {
     try {
-        return response.render('switchboard', {
+        return res.render('switchboard', {
+            user: req.oidc.user,
         })
     } catch(err) {
         console.log('Error:' + err);
     }
 });
-
-
-
-
+app.get("/sign-up/:page", (req, res) => {
+    const { page } = req.params;
+    res.oidc.login({
+        returnTo: page,
+        authorizationParams: {
+            screen_hint: "signup",
+        },
+    });
+});
+app.get("/login/:page", (req, res) => {
+    const { page } = req.params;
+    res.oidc.login({
+      returnTo: page,
+    });
+});
+app.get("/logout/:page", (req, res) => {
+    const { page } = req.params;
+    res.oidc.logout({
+      returnTo: page,
+    });
+});
+//app.get("/profile", (req, res) => {
+//    res.render("profile", {
+//      user: req.oidc.user,
+//    });
+//});
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 
 ///////////////////////////////////////////////////////////////////////////////////
 // For local testing only
@@ -109,7 +175,7 @@ app.listen(port, function(err) {
     console.log(`The server is running on port ${port}`);
     console.log(__dirname);
     console.log(__dirname + '/public');
-})
+});
 app.use((err, req, res, next) => {
     res.json(err);
 });
--- routes/portalRoutes.js
+++ routes/portalRoutes.js
@@ -47,40 +47,6 @@ router.get('/', async (request, response) => {
     }
 });
 
-router.get('/switchboard', /* requiresAuth(), */ async (request, response) => {
-
-        try {
-            // get a distinct list of Sponsors
-    //        const sponsorsAll = await Sponsors.SponsorsAll.findAll();
-    
-            // get a distinct list of Sponsors for the Search Criteria DDL
-    //        const sponsorsDDL = await Sponsors.SponsorsDDL.findAll();
-    
-            // get a distinct list of Sponsor Type(s) for the Search Criteria DDL
-    //        const sponsortypesDDL = await Sponsors.SponsorTypeCategoriesDDL.findAll();
-    
-            // get a distinct list of active Scholarships, ordered by Sponsor Name, Scholarship Name
-    //        const scholarshipsActive = await Scholarships.ScholarshipsActive.findAll(
-    //             {
-    //            order: [
-    //                ['ScholarshipName', 'ASC'],
-    //                ['SponsorName', 'ASC'],
-    //            ]
-    //        }
-    //        );
-    //        console.log(`Scholarships (Active) Count: ${scholarshipsActive.length}`);
-    
-            return response.render('switchboard', {
-    //            sponsors:        sponsorsAll,
-    //            sponsorsddl:     sponsorsDDL,
-    //            sponsortypesddl: sponsortypesDDL,
-    //            scholarships:    scholarshipsActive
-            })
-        } catch(err) {
-            console.log('Error:' + err);
-        }
-    });
-
 router.get('/userprofile', /* requiresAuth(), */ (request, response) => {
 //    response.send(`Hello ${req.oidc.user.sub}, this is the admin section.`);
 //    res.send(JSON.stringify(req.oidc.user));
