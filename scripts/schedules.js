474542be21e997f43bf57f9fdb5dd617bdfe5600
Jeff QF <JeffQF@fios.tech>
2021-09-23 12:40:40 -0700

    schedules.js typo

--- scripts/schedules.js
+++ scripts/schedules.js
@@ -4,7 +4,7 @@ const { SponsorsDDL, SponsorTypeCategoriesDDL } = require('../models/sequelize.j
 async function test_email() {
     let sponsorTypeCategoriesDDL = await SponsorTypeCategoriesDDL.findAndCountAll({});
     let sponsorTypeCategoriesDDLRows = sponsorTypeCategoriesDDL.rows;
-    console.log(sponsorTypesCategoriesDDLRows);
+    console.log(sponsorTypeCategoriesDDLRows);
     console.log('Starting email');
     let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Testing Scheduler Within Node.js',
             `This is a test email sent by the Cron To Go scheduler.`);
