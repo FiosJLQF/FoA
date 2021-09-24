d2b0e6c2f9cc9e8164b45f2db83be029948b08f0
Jeff QF <JeffQF@fios.tech>
2021-09-24 07:58:56 -0700

    schedules.js full borders

--- scripts/schedules.js
+++ scripts/schedules.js
@@ -8,15 +8,25 @@ async function test_email() {
     //console.log('Starting email');
     let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Testing Scheduler Within Node.js', '',
             `This is a <b>test email</b> sent by the <em>Cron To Go</em> <u>scheduler</u>. There are ${sponsorTypeCategoriesDDL.count} rows.<br><br>
-            <table style="border: 1px solid black;">
+            <table style="border: 1px solid black; border-collapse: collapse;">
               <tr>
                 <th style="border: 1px solid black;">GenderCategory</th>
                 <th style="border: 1px solid black;">GenderCategoryID</th>
                 <th style="border: 1px solid black;">RecordID</th>
                 <th style="border: 1px solid black;">GenderCategorySortOrder</th>
               </tr>
-            <tr><td>Yes</td><td>919001</td><td>1</td><td>1</td></tr>
-            <tr><td>No</td><td>919002</td><td>2</td><td>2</td></tr>
+              <tr>
+                <td style="border: 1px solid black;>Yes</td>
+                <td style="border: 1px solid black;>919001</td>
+                <td style="border: 1px solid black;>1</td>
+                <td style="border: 1px solid black;>1</td>
+              </tr>
+              <tr>
+                <td style="border: 1px solid black;>No</td>
+                <td style="border: 1px solid black;>919002</td>
+                <td style="border: 1px solid black;>2</td>
+                <td style="border: 1px solid black;>2</td>
+              </tr>
             </table>`);
     //console.log('Finishing email');
 };
