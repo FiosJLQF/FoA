1578ec513c1b8da3eec0a56f88e500ada676876f
Jeff QF <JeffQF@fios.tech>
2021-09-24 08:48:40 -0700

    schedules.js test 2

--- scripts/schedules.js
+++ scripts/schedules.js
@@ -7,27 +7,9 @@ async function test_email() {
     // console.log(sponsorTypeCategoriesDDLRows);
     //console.log('Starting email');
     let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Testing Scheduler Within Node.js', '',
-            `This is a <b>test email</b> sent by the <em>Cron To Go</em> <u>scheduler</u>. There are ${sponsorTypeCategoriesDDL.count} rows.<br><br>
-            <table style="border: 1px solid black; border-collapse: collapse;">
-              <tr>
-                <th style="border: 1px solid black;">GenderCategory</th>
-                <th style="border: 1px solid black;">GenderCategoryID</th>
-                <th style="border: 1px solid black;">RecordID</th>
-                <th style="border: 1px solid black;">GenderCategorySortOrder</th>
-              </tr>
-              <tr>
-                <td style="border: 1px solid black;">Yes</td>
-                <td style="border: 1px solid black;">919001</td>
-                <td style="border: 1px solid black;">1</td>
-                <td style="border: 1px solid black;">1</td>
-              </tr>
-              <tr>
-                <td style="border: 1px solid black;">No</td>
-                <td style="border: 1px solid black;">919002</td>
-                <td style="border: 1px solid black;">2</td>
-                <td style="border: 1px solid black;">2</td>
-              </tr>
-            </table>`);
+      `This is a <b>test email</b> sent by the <em>Cron To Go</em> <u>scheduler</u>. There are ${sponsorTypeCategoriesDDL.count} rows.<br><br>
+      <table style="border: 1px solid black; border-collapse: collapse;"><tr><th style="border: 1px solid black;">Description</th><th style="border: 1px solid black;">Scholarship Name</th><th style="border: 1px solid black;">Sponsor Name</th><th style="border: 1px solid black;">Recurrence</th><th style="border: 1px solid black;">List Date</th><th style="border: 1px solid black;">Start Date</th><th style="border: 1px solid black;">End Date</th></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Student Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Conference Assistance Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Loretta Scott, A.A.E. Accreditation/Certification Academy Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr></table>`
+    );
     //console.log('Finishing email');
 };
 
