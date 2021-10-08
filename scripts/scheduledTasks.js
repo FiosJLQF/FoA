89cf2591f39107f0f1fe94a5a2e2129eb2c2b377
Jeff QF <JeffQF@fios.tech>
2021-10-08 13:13:43 -0700

    scheduledTasks added upcoming scholarships

--- scripts/scheduledTasks.js
+++ scripts/scheduledTasks.js
@@ -6,7 +6,7 @@ const jsFx = require('./foa_node_fx');
 **************************************************************************************************/
 
 
-async function scholarshipDates() {
+async function checkTasksToRun() {
 //    let sponsorTypeCategoriesDDL = await SponsorTypeCategoriesDDL.findAndCountAll({});
 //    let sponsorTypeCategoriesDDLRows = sponsorTypeCategoriesDDL.rows;
 //    let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Testing Scheduler Within Node.js', '',
@@ -14,37 +14,58 @@ async function scholarshipDates() {
 //      <table style="border: 1px solid black; border-collapse: collapse;"><tr><th style="border: 1px solid black;">Description</th><th style="border: 1px solid black;">Scholarship Name</th><th style="border: 1px solid black;">Sponsor Name</th><th style="border: 1px solid black;">Recurrence</th><th style="border: 1px solid black;">List Date</th><th style="border: 1px solid black;">Start Date</th><th style="border: 1px solid black;">End Date</th></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Student Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Conference Assistance Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr><tr><td style="border: 1px solid black;">Scholarships With No Application Dates</td><td style="border: 1px solid black;">Loretta Scott, A.A.E. Accreditation/Certification Academy Scholarship</td><td style="border: 1px solid black;">South Central Chapter - American Association of Airport Executives</td><td style="border: 1px solid black;">Annual</td></tr></table>`
 //    );
 
+  ////////////////////////////////////////////
+  // Get "today's" date for schedule checks
+  ////////////////////////////////////////////
   var today = new Date();
 
-    if ( today.getDate() === 1 ) { // Run on the 1st of the month
-      const { Sequelize, Model, DataTypes } = require('sequelize');
-      const sequelize = new Sequelize( process.env.DB_URI, {
-          dialect: 'postgres',
-          operatorsAliases: 0,
-          dialectOptions: {
-              ssl: {
-                require: true,
-                rejectUnauthorized: false
-              }
+  ////////////////////////////////////////////
+  // Initialize the data connection for use in retrieving data for notifications
+  ////////////////////////////////////////////
+  const { Sequelize, Model, DataTypes } = require('sequelize');
+  const sequelize = new Sequelize( process.env.DB_URI, {
+      dialect: 'postgres',
+      operatorsAliases: 0,
+      dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
           }
-      });
+      }
+  });
 
-      sequelize.query(
-          `SELECT public."fnQueryToHTMLTable"('SELECT * FROM public."vwScholarships_DateProblems_Alert"') AS "TableDef"`,
-          { type: sequelize.QueryTypes.SELECT })
-      .then( htmlTable => {
-        console.log(`Date Issues: ${htmlTable[0].TableDef}`);
-          let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Scholarship Date Notifications', '',
-            htmlTable[0].TableDef);
-      });
-    } else {
-      let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', `Today is the ${today.getDate()} day of the month`, '', '');
-    };
+  ////////////////////////////////////////////
+  // Check the schedules for tasks to run
+  ////////////////////////////////////////////
+  if ( today.getDate() === 1 ) { // Run on the 1st of the month
+    let scholarshipDatesResult = scholarshipDates(today, sequelize);
+  } else {
+    let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', `Today is the ${today.getDate()} day of the month`, '', '');
+  };
 
-};
+}; // end checkTasksToRun
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Scholarship Dates (date issues, upcoming Scholarships)
+//////////////////////////////////////////////////////////////////////////////////////////
+function scholarshipDates(today, sequelize) {
+
+  sequelize.query(
+    `SELECT public."fnQueryToHTMLTable"('SELECT * FROM public."vwAlert_ScholarshipDates"') AS "TableDef"`,
+    { type: sequelize.QueryTypes.SELECT })
+  .then( async htmlTable => {
+    // send the email notification
+    let emailResultError = jsFx.sendEmail('fiosjlqf@gmail.com', 'Scholarship Date Notifications', '',
+      htmlTable[0].TableDef);
+    // log the event
+    let logEventResult = await jsFx.logEvent('Scheduled Task', 'scholarshipDates', 0,
+      'Success', '', 0, 0, 0, process.env.EMAIL_WEBMASTER_LIST);
+  });
+
+}; // end scholarshipDates
 
 module.exports = {
-  scholarshipDates
+  checkTasksToRun
 };
 
 require('make-runnable');
